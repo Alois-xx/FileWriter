cfc602c49cd6e2ddad7b62b6cdaa5d6905836ecc
Alois-xx <akraus1@gmx.de>
2022-08-24 22:01:38 +0200

    Delete files before profiling starts to bring NTFS into a well defined state

--- RunPerformanceTests.cmd
+++ RunPerformanceTests.cmd
@@ -1,13 +1,27 @@
 @echo off
 REM with powershell you can add a Defender exclusion directory with Add-MpPreference -ExclusionPath "C:\temp\Test"
 REM to check AV overhead with and without AV interference
-set OutDir=C:\temp\Test1
+set OutDir=C:\temp\Testdata
+
+call :CleanFiles
 call ProfileTest.cmd CreateSerial       FileWriter.exe -generate "%OutDir%" serial
+call :CleanFiles
 call ProfileTest.cmd CreateParallel     FileWriter.exe -generate "%OutDir%" parallel
+call :CleanFiles
 call ProfileTest.cmd CreateParallel2    FileWriter.exe -generate "%OutDir%" parallel -threads 2
+call :CleanFiles
 call ProfileTest.cmd CreateParallel3    FileWriter.exe -generate "%OutDir%" parallel -threads 3
+call :CleanFiles
 call ProfileTest.cmd CreateParallel4    FileWriter.exe -generate "%OutDir%" parallel -threads 4
+call :CleanFiles
 call ProfileTest.cmd CreateParallel5    FileWriter.exe -generate "%OutDir%" parallel -threads 5
+call :CleanFiles
 call ProfileTest.cmd CreateTaskParallel FileWriter.exe -generate "%OutDir%" taskparallel
+call :CleanFiles
 
 call ExtractETW.cmd
+goto :EOF
+
+:CleanFiles
+for %%i in (%OutDir%\*.cmd) do del %%i
+goto :EOF
