489f8784069f3eaf93ed59e8992ce070ef2edba7
Alois-xx <akraus1@gmx.de>
2025-05-08 09:17:12 +0200

    RunPerformanceTests.cmd supports    as first argument ETW output folder and second option the number of parallel extraction processes to control memory consumption during extraction.    Additionally it supports now different test cases in script to make it easy to adapt it to custom scenarios.

--- ExtractETW.cmd
+++ ExtractETW.cmd
@@ -1,12 +1,22 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
 where /Q ETWAnalyzer
 if %ERRORLEVEL% GTR 0 (
 	echo Install latest ETWAnalyzer from https://github.com/Siemens-Healthineers/ETWAnalyzer/releases/ and put it in your path.
 	goto :EOF
 )
+set TestCase=%1
 REM Extract all CPU data (-allcpu) which can make file size ca. 50% larger but then you see everything what WPA also sees
 REM skip already exctracted files 
-ETWAnalyzer -extract all -fd c:\temp\*Create*.etl -symserver ms -nooverwrite
-IF %ERRORLEVEL% LEQ 0 (
-	echo If during extraction files could not be extracted due to OutOfMemoryException you can execute the ExtractETW script again to extract the missing files. 
-	echo You can also add to the ETWAnalyzer call -nthreads 1 to force single threaded extraction.
+
+set ThreadArgs=
+if "!ThreadCount!" NEQ "" set ThreadArgs=-nthreads !ThreadCount!
+
+ETWAnalyzer -extract all -fd !ETWLocation!\*%TestCase%*.etl -symserver ms -nooverwrite !ThreadArgs!
+IF %ERRORLEVEL% LSS 0 (
+	echo If during extraction files could not be extracted due to OutOfMemoryException you can execute the RunPerformanceTests.cmd  
+	echo with RunPerformanceTests c:\temp\ETW 1 to limit extraction to one parallel instance during extraction. The first argument is the output folder where 
+	echo the first argument is the ETW data folder and second argument is the number of parallel extractor instances. 
+)
+
+goto :EOF
--- RunPerformanceTests.cmd
+++ RunPerformanceTests.cmd
@@ -1,27 +1,64 @@
 @echo off
-REM with powershell you can add a Defender exclusion directory with Add-MpPreference -ExclusionPath "C:\temp\Test"
-REM to check AV overhead with and without AV interference
-set OutDir=C:\temp\Testdata
+setlocal enabledelayedexpansion
 
+REM set TestCase=NugetDownload
+REM set OutDir=%userprofile%\.nuget\packages\FileWriterTestFolder
+
+set TestCase=CreateParallel
+set OutDir=%temp%\FileCreationTest
+
+set ETWLocation=C:\Temp
+
+setlocal enabledelayedexpansion
+set ScriptLocation=%~dp0
+
+
+REM First argument is directory where ETW files are written to. Default is C:\temp
+REM Second argument is the number of concurrent ETW extractions. Default is up to 5 depending on core count. That can lead to extraction errors due to not enough 
+REM memory. In that case add as second argument to the script 1 to try a slower but less memory hungry extraction.
+
+if "%1" NEQ "" set ETWLocation=%1
+if "%2" NEQ "" set ThreadCount=%2
+echo Saving ETW Files to !ETWLocation!, Parallel Extractors: !ThreadCount!
+
+REM Simulate Nuget Download to user local folder 
+
+REM set Extension=.dll
+REM call :CleanFiles
+REM call !ScriptLocation!ProfileTest.cmd %TestCase%-%Extension:~1%  "!ScriptLocation!FileWriter.exe" -generate "%OutDir%" serial -files 500 -extension %Extension%
+REM call :CleanFiles
+REM 
+REM set Extension=.exe
+REM call !ScriptLocation!ProfileTest.cmd %TestCase%-%Extension:~1%  "!ScriptLocation!FileWriter.exe" -generate "%OutDir%" serial -files 500 -extension %Extension%
+REM call :CleanFiles
+REM 
+REM set Extension=.ps1
+REM call !ScriptLocation!ProfileTest.cmd %TestCase%-%Extension:~1%  "!ScriptLocation!FileWriter.exe" -generate "%OutDir%" serial -files 500 -extension %Extension%
+REM call :CleanFiles
+
+REM Simulate file writing of .cmd files to a folder which might be excluded by AV or not
+set Extension=.cmd
 call :CleanFiles
-call ProfileTest.cmd CreateSerial       FileWriter.exe -generate "%OutDir%" serial
+call ProfileTest.cmd CreateParallel     FileWriter.exe -generate "%OutDir%" parallel -extension %Extension%
 call :CleanFiles
-call ProfileTest.cmd CreateParallel     FileWriter.exe -generate "%OutDir%" parallel
+call ProfileTest.cmd CreateParallel2    FileWriter.exe -generate "%OutDir%" parallel -threads 2 -extension %Extension%
 call :CleanFiles
-call ProfileTest.cmd CreateParallel2    FileWriter.exe -generate "%OutDir%" parallel -threads 2
+call ProfileTest.cmd CreateParallel3    FileWriter.exe -generate "%OutDir%" parallel -threads 3 -extension %Extension%
 call :CleanFiles
-call ProfileTest.cmd CreateParallel3    FileWriter.exe -generate "%OutDir%" parallel -threads 3
+call ProfileTest.cmd CreateParallel4    FileWriter.exe -generate "%OutDir%" parallel -threads 4 -extension %Extension%
 call :CleanFiles
-call ProfileTest.cmd CreateParallel4    FileWriter.exe -generate "%OutDir%" parallel -threads 4
-call :CleanFiles
-call ProfileTest.cmd CreateParallel5    FileWriter.exe -generate "%OutDir%" parallel -threads 5
-call :CleanFiles
-call ProfileTest.cmd CreateTaskParallel FileWriter.exe -generate "%OutDir%" taskparallel
+call ProfileTest.cmd CreateParallel5    FileWriter.exe -generate "%OutDir%" parallel -threads 5 -extension %Extension%
 call :CleanFiles
 
-call ExtractETW.cmd
+REM delete everything in output folder
+if "%OutDir%" NEQ "" (
+	rd /q /s "%OutDir%"
+)
+
+call !ScriptLocation!ExtractETW.cmd %TestCase%
+REM ETWAnalyzer -dump CPU -pn FileWriter -stacktags *Virus* -methods RtlUserThreadStart -fd !ETWLocation!\Extract\*%TestCase%*.json7z
 goto :EOF
 
 :CleanFiles
-for %%i in (%OutDir%\*.cmd) do del %%i
+for %%i in ("%OutDir%\*!Extension!") do del %%i
 goto :EOF
