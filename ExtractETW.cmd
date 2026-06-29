103bb3768d45dd4f1d87c1d7f2a1fee519c73856
Alois-xx <akraus1@gmx.de>
2026-06-29 14:16:28 +0200

    Support local ETWAnalyzer instance below the script folder.

--- ExtractETW.cmd
+++ ExtractETW.cmd
@@ -1,19 +1,27 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
-where /Q ETWAnalyzer
-if %ERRORLEVEL% GTR 0 (
-	echo Install latest ETWAnalyzer from https://github.com/Siemens-Healthineers/ETWAnalyzer/releases/ and put it in your path.
-	goto :EOF
+set ScriptLocation=%~dp0
+if EXIST "!ScriptLocation!ETWAnalyzer\ETWAnalyzer.exe" (
+	set ETWAnalyzer=!ScriptLocation!ETWAnalyzer\ETWAnalyzer.exe
+) ELSE (
+	where /Q ETWAnalyzer
+	if %ERRORLEVEL% GTR 0 (
+		echo Install latest ETWAnalyzer from https://github.com/Siemens-Healthineers/ETWAnalyzer/releases/ and put it in your path, or deploy ETWAnalyzer in a subfolder of the script named ETWAnalzyer.
+		goto :EOF
+	)
+	set ETWAnalyzer=ETWAnalyzer
 )
+
 set ETWLocation=%1
 set TestCase=%2
+
 REM Extract all CPU data (-allcpu) which can make file size ca. 50% larger but then you see everything what WPA also sees
 REM skip already exctracted files 
 
 set ThreadArgs=
 if "!ThreadCount!" NEQ "" set ThreadArgs=-nthreads !ThreadCount!
 
-ETWAnalyzer -extract all -fd !ETWLocation!\*%TestCase%*.etl -symserver ms -nooverwrite !ThreadArgs!
+"!ETWAnalyzer!" -extract all -fd !ETWLocation!\*%TestCase%*.etl -symserver ms -nooverwrite !ThreadArgs!
 IF %ERRORLEVEL% LSS 0 (
 	echo If during extraction files could not be extracted due to OutOfMemoryException you can execute the RunPerformanceTests.cmd  
 	echo with RunPerformanceTests c:\temp\ETW 1 to limit extraction to one parallel instance during extraction. The first argument is the output folder where 
